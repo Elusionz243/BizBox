a51aa2eb9c6b5a4dc4449f7ef5bc7388494fe5b8
Elusionz243 <strivelinkband@yahoo.com>
2024-03-13 21:28:42 -0500

    Updated report system, made it more modular and 'build it yourself' like

--- front-end/src/components/apps/reports/Reports.js
+++ front-end/src/components/apps/reports/Reports.js
@@ -4,163 +4,74 @@ import FormGen from "../utils/FormGen";
 
 import "./Reports.scss";
 import Icon from "../../utils/icon/Icon";
-export default function Reports({}) {
-  const expenseReport = {
-    title: "Expense Upload Form",
-    structure: [
-      {
-        type: "text",
-        title: "Purchaser's Name",
-        description: "Who placed the order?",
-        options: [],
-      },
-      {
-        type: "select",
-        title: "Store",
-        description: "Select the store the purchase was for.",
-        options: [
-          "#1 Fredericksburg",
-          "#2 San Pedro",
-          "#3 HWY 151",
-          "#4 Wurzbach",
-          "#5 Shaenfield",
-          "#6 Huebner",
-          "#8 Potranco",
-          "#9 Dominion",
-          "#10 Blanco",
-          "#11 TPC",
-          "#12 Stone Oak",
-        ],
-      },
-      {
-        type: "select",
-        title: "Vendor",
-        description: "Where was the purchase made?",
-        options: [
-          "Skyco",
-          "Texas Wholesale",
-          "Amazon",
-          "Walmart",
-          "Fincks",
-          "Medusa",
-          "Sam's Club",
-          "Unknown",
-          "Other",
-        ],
-      },
-      {
-        type: "select",
-        title: "Expense Type",
-        description: "What is the purpose of this purchase?",
-        options: [
-          "Inventory Restock",
-          "Store Supplies & Business Equipment",
-          "Snacks and personal care items",
-          "Decorations",
-          "Maintenance",
-          "Rent & Bills",
-          "Other",
-        ],
-      },
-      {
-        type: "date",
-        title: "Date of Purchase",
-        description:
-          "When was the purchase made? (Weekend skyco orders must be submitted on the following Monday)",
-        options: [],
-      },
-      {
-        type: "select",
-        title: "Payment Method",
-        description: "How was the purchase paid for?",
-        options: [
-          "Cash",
-          "Credit Card",
-          "Debit Card",
-          "Check",
-          "Plum Amex",
-          "Other",
-        ],
-      },
-      {
-        type: "text",
-        title: "Receipt Total",
-        description:
-          "List the payment total on the receipt, including taxes, shipping, etc.",
-        options: [],
-      },
-      {
-        type: "text",
-        title: "Sales Tax Total",
-        description: "How much did we pay in sales tax on this order?",
-        options: [],
-      },
-      {
-        type: "file",
-        title: "Receipt / Invoice Upload",
-        description:
-          "Upload a copy of the receipt or invoice for this purchase.",
-        options: [],
-      },
-    ],
-  };
+import reportBuilderStructure from "../../../utils/reportBuilder.json";
 
+export default function Reports({}) {
+  const reportBuilder = reportBuilderStructure[0];
+  const expenseReport = reportBuilderStructure[1];
+  const groupStructure = reportBuilderStructure[2];
   const initialGroupList = [
     {
       title: "Finances",
-      reports: ["Expense Upload Form", "View Submitted Expenses"],
+      reports: [
+        { title: "Expense Upload Form", structure: [expenseReport] },
+        { title: "View Submitted Expenses", structure: [] },
+      ],
     },
     {
       title: "Inventory",
       reports: [
-        "Thrive Product Category Guide",
-        "Product Loss Upload Form",
-        "This Month's Submitted Losses",
-        "Last Month's Submitted Losses",
+        { title: "Thrive Product Category Guide", structure: [] },
+        { title: "Product Loss Upload Form", structure: [] },
+        { title: "This Month's Submitted Losses", structure: [] },
+        { title: "Last Month's Submitted Losses", structure: [] },
       ],
     },
     {
       title: "Human Resources",
       reports: [
-        "Training Manuals",
-        "Anonymous Complaint Form",
-        "Employee Write Up Form",
-        "Manager Review Form",
-        "Employee Review Form",
-        "Employee Review Entries",
+        { title: "Training Manuals", structure: [] },
+        { title: "Anonymous Complaint Form", structure: [] },
+        { title: "Employee Write Up Form", structure: [] },
+        { title: "Manager Review Form", structure: [] },
+        { title: "Employee Review Form", structure: [] },
+        { title: "Employee Review Entries", structure: [] },
       ],
     },
     {
       title: "Property Management",
-      reports: ["Maintenance Request Form", "Maintenance Queue"],
+      reports: [
+        { title: "Maintenance Request Form", structure: [] },
+        { title: "Maintenance Queue", structure: [] },
+      ],
     },
     {
       title: "Extra Files",
-      reports: ["Simple Tip Calculator", "Hazel Sky Logo Files"],
+      reports: [
+        { title: "Simple Tip Calculator", structure: [] },
+        { title: "Hazel Sky Logo Files", structure: [] },
+      ],
     },
   ];
-
-  const groupListStructure = {
-    title: "Create Group",
-    structure: [
-      {
-        type: "text",
-        title: "Group Name",
-        description: "Enter the name of the group you want to create.",
-        options: [],
-      },
-    ],
-  };
 
   const [groupList, setGroupList] = useState([...initialGroupList]);
   const [reportList, setReportList] = useState([]);
   const [uploadedReports, setUploadedReports] = useState([]);
+
   const [formStructure, setFormStructure] = useState({});
-  const [openGroup, setOpenGroup] = useState();
   const [formData, setFormData] = useState({});
+
+  const [showGroupList, setShowGroupList] = useState(true);
 
   const openCategory = (index) => {
     setReportList(groupList[index].reports);
+    setShowGroupList(!showGroupList);
+  };
+
+  const openReport = (structure) => {
+    if (!structure) return;
+    setFormStructure({ ...structure[0] });
+    // setShowGroupList(!showGroupList);
   };
 
   const addGroup = (e) => {
@@ -171,13 +82,22 @@ export default function Reports({}) {
     ]);
     setFormData({});
   };
+
+  const openReportBuilder = () => {
+    setFormStructure({ ...reportBuilder });
+    // setFormData({});
+  };
   return (
     <div className="reports">
-      <div className="reports__nav">
-        <div className="reports__nav-buttons">
+      <div className="reports-nav">
+        <div className="reports-nav__buttons">
           <div
-            className="reports__add-group"
-            onClick={() => setFormStructure({ ...groupListStructure })}
+            className="reports-nav__add-group"
+            onClick={() =>
+              showGroupList
+                ? setFormStructure({ ...groupStructure })
+                : openReportBuilder()
+            }
           >
             <div>
               <Icon
@@ -188,20 +108,39 @@ export default function Reports({}) {
                 viewBox={"0 0 16 16"}
               />
             </div>
-            <div className="reports__add-group-text">Add Group</div>
+            <div className="reports-nav__add-group-text">
+              {showGroupList ? "Add Group" : "Add Report"}
+            </div>
           </div>
         </div>
-        {groupList.length > 0 &&
-          groupList.map(({ title }, index) => (
-            <div
-              className="reports__button"
-              onClick={() => openCategory(index)}
-            >
-              {title}
-            </div>
-          ))}
+        {!showGroupList && (
+          <div
+            className="reports-nav__button"
+            onClick={(e) => setShowGroupList(!showGroupList)}
+          >
+            Back
+          </div>
+        )}
+        {showGroupList && groupList.length > 0
+          ? groupList.map(({ title }, index) => (
+              <div
+                className="reports-nav__button"
+                onClick={() => openCategory(index)}
+              >
+                {title}
+              </div>
+            ))
+          : reportList.length > 0 &&
+            reportList.map(({ title, structure }) => (
+              <div
+                className="reports-nav__button"
+                onClick={(e) => openReport(structure)}
+              >
+                {title}
+              </div>
+            ))}
       </div>
-      <div className="reports__content">
+      <div className="reports-content">
         {formStructure.structure && formStructure.structure.length > 0 ? (
           <FormGen
             formStructure={formStructure}
@@ -209,7 +148,11 @@ export default function Reports({}) {
             formData={formData}
             setFormData={setFormData}
           />
-        ) : null}
+        ) : (
+          <div className="reports-content__dashboard">
+            <div className="reports-content__dashboard-title">Reports</div>
+          </div>
+        )}
       </div>
     </div>
   );
